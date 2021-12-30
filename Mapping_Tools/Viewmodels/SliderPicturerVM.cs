1a595d28ddaae5cadd64c63fa919d401886a1b43
Jonathan Keegan <jonathan.keegan@intellibus.com>
2021-12-29 21:21:28 -0500

    Slider Picturer Template Code

---
+++ Mapping_Tools/Viewmodels/SliderPicturerVM.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Input;
+using Mapping_Tools.Classes.SystemTools;
+using Newtonsoft.Json;
+
+namespace Mapping_Tools.Viewmodels
+{
+    public class SliderPicturerVM : BindableBase
+    {
+        #region Properties
+
+        [JsonIgnore]
+        public string[] Paths { get; set; }
+
+        [JsonIgnore]
+        public bool Quick { get; set; }
+
+        private ImportMode _importModeSetting;
+        public ImportMode ImportModeSetting
+        {
+            get => _importModeSetting;
+            set
+            {
+                if (Set(ref _importModeSetting, value)) {
+                    RaisePropertyChanged(nameof(TimeCodeBoxVisibility));
+                }
+            }
+        }
+
+        [JsonIgnore]
+        public IEnumerable<ImportMode> ImportModes => Enum.GetValues(typeof(ImportMode)).Cast<ImportMode>();
+
+        [JsonIgnore]
+        public Visibility TimeCodeBoxVisibility => ImportModeSetting == ImportMode.Time ? Visibility.Visible : Visibility.Collapsed;
+
+        private FreeVariable _freeVariableSetting;
+        public FreeVariable FreeVariableSetting
+        {
+            get => _freeVariableSetting;
+            set
+            {
+                if (Set(ref _freeVariableSetting, value)) {
+                    RaisePropertyChanged(nameof(DurationBoxVisibility));
+                    RaisePropertyChanged(nameof(EndTimeBoxVisibility));
+                    RaisePropertyChanged(nameof(LengthBoxVisibility));
+                    RaisePropertyChanged(nameof(VelocityBoxVisibility));
+                }
+            }
+        }
+
+        [JsonIgnore]
+        public IEnumerable<FreeVariable> FreeVariables => Enum.GetValues(typeof(FreeVariable)).Cast<FreeVariable>();
+
+        [JsonIgnore]
+        public Visibility DurationBoxVisibility => FreeVariableSetting != FreeVariable.Duration && !UseEndTime ? Visibility.Visible : Visibility.Collapsed;
+
+        [JsonIgnore]
+        public Visibility EndTimeBoxVisibility => FreeVariableSetting != FreeVariable.Duration && UseEndTime ? Visibility.Visible : Visibility.Collapsed;
+
+        [JsonIgnore]
+        public Visibility LengthBoxVisibility => FreeVariableSetting != FreeVariable.Length ? Visibility.Visible : Visibility.Collapsed;
+
+        [JsonIgnore]
+        public Visibility VelocityBoxVisibility => FreeVariableSetting != FreeVariable.Velocity ? Visibility.Visible : Visibility.Collapsed;
+
+        private string _timeCode;
+        public string TimeCode
+        {
+            get => _timeCode;
+            set => Set(ref _timeCode, value);
+        }
+
+        private double _duration;
+        public double Duration
+        {
+            get => _duration;
+            set => Set(ref _duration, value);
+        }
+
+        private double _endTime;
+        public double EndTime
+        {
+            get => _endTime;
+            set => Set(ref _endTime, value);
+        }
+
+        private double _length;
+        public double Length
+        {
+            get => _length;
+            set => Set(ref _length, value);
+        }
+
+        private double _sliderVelocity;
+        public double SliderVelocity
+        {
+            get => _sliderVelocity;
+            set => Set(ref _sliderVelocity, value);
+        }
+
+        private bool _moveAnchors;
+        public bool MoveAnchors
+        {
+            get => _moveAnchors;
+            set => Set(ref _moveAnchors, value);
+        }
+
+        private bool _useEndTime;
+        public bool UseEndTime
+        {
+            get => _useEndTime;
+            set
+            {
+                if (Set(ref _useEndTime, value)) {
+                    RaisePropertyChanged(nameof(DurationBoxVisibility));
+                    RaisePropertyChanged(nameof(EndTimeBoxVisibility));
+                }
+            }
+        }
+
+        private bool _delegateSvToBpm;
+        public bool DelegateToBpm
+        {
+            get => _delegateSvToBpm;
+            set => Set(ref _delegateSvToBpm, value);
+        }
+
+        private bool _removeSliderTicks;
+        public bool RemoveSliderTicks
+        {
+            get => _removeSliderTicks;
+            set => Set(ref _removeSliderTicks, value);
+        }
+
+        private ICommand _uploadFile;
+        public ICommand UploadFile
+        {
+            get
+            {
+                return _uploadFile ?? (_uploadFile = new CommandHandler(() => SetFile(), () => true));
+            }
+        }
+
+        private String _pictureFile;
+        public String PictureFile
+        {
+            get => _pictureFile;
+            set => Set(ref _pictureFile, value);
+        }
+
+        private void SetFile()
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.DefaultExt = ".png"; // Required file extension 
+            fileDialog.Filter = @"All Image Files|*.BMP;*.bmp;*.JPG;*.JPEG*.jpg;*.jpeg;*.PNG;*.png;*.GIF;*.gif;*.tif;*.tiff;*.ico;*.ICO|PNG|*.PNG;*.png|JPEG|*.JPG;*.JPEG*.jpg;*.jpeg|Bitmap(.BMP,.bmp)|*.BMP;*.bmp|GIF|*.GIF;*.gif|TIF|*.tif;*.tiff|ICO|*.ico;*.ICO";// Optional file extensions
+
+            if (fileDialog.ShowDialog() == DialogResult.OK) {
+                PictureFile = fileDialog.FileName;
+            }
+        }
+
+        #endregion
+
+        public SliderPicturerVM()
+        {
+            ImportModeSetting = ImportMode.Selected;
+            Duration = -1;
+            EndTime = -1;
+            Length = 1;
+            SliderVelocity = -1;
+            MoveAnchors = false;
+            UseEndTime = false;
+            DelegateToBpm = false;
+            RemoveSliderTicks = false;
+            TimeCode = string.Empty;
+
+        }
+
+        public enum FreeVariable
+        {
+            Velocity,
+            Length,
+            Duration
+        }
+
+        public enum ImportMode
+        {
+            Selected,
+            Bookmarked,
+            Time,
+            Everything
+        }
+    }
+    
+}
---
+++ Mapping_Tools/Views/SliderPicturer/SliderPicturerView.xaml.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+using System.Drawing;
+using Mapping_Tools.Classes;
+using Mapping_Tools.Classes.BeatmapHelper;
+using Mapping_Tools.Classes.MathUtil;
+using Mapping_Tools.Classes.SystemTools;
+using Mapping_Tools.Classes.SystemTools.QuickRun;
+using Mapping_Tools.Classes.ToolHelpers;
+using Mapping_Tools.Classes.Tools;
+using Mapping_Tools.Viewmodels;
+
+namespace Mapping_Tools.Views.SliderPicturer {
+    /// <summary>
+    /// Interaktionslogik für UserControl1.xaml
+    /// </summary>
+    [SmartQuickRunUsage(SmartQuickRunTargets.AnySelection)]
+    [VerticalContentScroll]
+    [HorizontalContentScroll]
+    public partial class SliderPicturerView : IQuickRun, ISavable<SliderPicturerVM> {
+        public event EventHandler RunFinished;
+
+        public static readonly string ToolName = "Slider Picturer";
+
+        public static readonly string ToolDescription = $@"Change the length and duration of marked sliders and this tool will automatically handle the SliderVelocity for you.";
+
+        /// <inheritdoc />
+        public SliderPicturerView() {
+            InitializeComponent();
+            Width = MainWindow.AppWindow.content_views.Width;
+            Height = MainWindow.AppWindow.content_views.Height;
+            DataContext = new SliderPicturerVM();
+            ProjectManager.LoadProject(this, message: false);
+        }
+
+        public SliderPicturerVM ViewModel => (SliderPicturerVM) DataContext;
+
+        protected override void BackgroundWorker_DoWork(object sender, DoWorkEventArgs e) {
+            var bgw = sender as BackgroundWorker;
+            e.Result = Picturate((SliderPicturerVM) e.Argument, bgw, e);
+        }
+
+       
+        private void Start_Click(object sender, RoutedEventArgs e) {
+            // Get the current beatmap if the selection mode is 'Selected' because otherwise the selection would always fail
+            RunTool(SelectionModeBox.SelectedIndex == 0
+                ? new[] {IOHelper.GetCurrentBeatmapOrCurrentBeatmap()}
+                : MainWindow.AppWindow.GetCurrentMaps());
+        }
+
+        public void QuickRun() {
+            RunTool(new[] { IOHelper.GetCurrentBeatmapOrCurrentBeatmap() }, quick: true);
+        }
+
+        private void RunTool(string[] paths, bool quick = false) {
+            if (!CanRun) return;
+
+            // Remove logical focus to trigger LostFocus on any fields that didn't yet update the ViewModel
+            FocusManager.SetFocusedElement(FocusManager.GetFocusScope(this), null);
+
+            BackupManager.SaveMapBackup(paths);
+
+            ViewModel.Paths = paths;
+            ViewModel.Quick = quick;
+
+            BackgroundWorker.RunWorkerAsync(ViewModel);
+            CanRun = false;
+        }
+
+        private string Picturate(SliderPicturerVM arg, BackgroundWorker worker, DoWorkEventArgs _) {
+            int slidersCompleted = 0;
+
+            var reader = EditorReaderStuff.GetFullEditorReaderOrNot(out var editorReaderException1);
+
+            if (arg.PictureFile == null) {
+                throw new Exception("No image file selected.", editorReaderException1);
+            }
+
+            Bitmap img;
+            try {
+                img = new Bitmap(arg.PictureFile);
+            } catch {
+                throw new Exception("Not a valid image file.");
+            }
+
+            // Recolor bitmap according to slider colors
+            // TODO: get actual color values
+            // Slider border is always completely opaque (alpha=255)
+            Color sliderBorder = Color.FromArgb(255, 255, 255, 255);
+            Color sliderColor = Color.FromArgb(0,0,0);
+            Color backgroundColor = Color.FromArgb(0,0,0);
+            const double LIGHTEN_AMOUNT = 0.5;
+            const double DARKEN_AMOUNT = 0.1;
+            const byte ALPHA = 100;
+            Color innerColor = Color.FromArgb(ALPHA,
+                (byte)Math.Min(255, sliderColor.R * (1 + 0.5 * LIGHTEN_AMOUNT) + 255 * LIGHTEN_AMOUNT),
+                (byte)Math.Min(255, sliderColor.G * (1 + 0.5 * LIGHTEN_AMOUNT) + 255 * LIGHTEN_AMOUNT),
+                (byte)Math.Min(255, sliderColor.B * (1 + 0.5 * LIGHTEN_AMOUNT) + 255 * LIGHTEN_AMOUNT));
+            Color outerColor = Color.FromArgb(ALPHA,
+                (byte)Math.Min(255, sliderColor.R / (1 + DARKEN_AMOUNT)),
+                (byte)Math.Min(255, sliderColor.G / (1 + DARKEN_AMOUNT)),
+                (byte)Math.Min(255, sliderColor.B / (1 + DARKEN_AMOUNT)));
+
+            // TODO: 
+
+            // Complete progressbar
+            if (worker != null && worker.WorkerReportsProgress)
+            {
+                worker.ReportProgress(100);
+            }
+
+            // Do stuff
+            RunFinished?.Invoke(this, new RunToolCompletedEventArgs(true, reader != null, arg.Quick));
+
+            // Make an accurate message
+            string message = "";
+            if (Math.Abs(slidersCompleted) == 1)
+            {
+                message += "Successfully completed " + slidersCompleted + " slider!";
+            }
+            else
+            {
+                message += "Successfully completed " + slidersCompleted + " sliders!";
+            }
+            return arg.Quick ? "" : message;
+        }
+        public SliderPicturerVM GetSaveData() {
+            return ViewModel;
+        }
+
+        public void SetSaveData(SliderPicturerVM saveData) {
+            DataContext = saveData;
+        }
+
+        public string AutoSavePath => Path.Combine(MainWindow.AppDataPath, "sliderPicturerproject.json");
+
+        public string DefaultSaveFolder => Path.Combine(MainWindow.AppDataPath, "Slider Picturer Projects");
+    }
+}
